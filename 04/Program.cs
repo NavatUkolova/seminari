9fc7128aba1dafa4b1be402ea76ee3c11e9f1c97
Ukolova Nabat <ru.nabat@gmail.com>
2022-04-30 20:31:44 +0400

    zadacha04

---
+++ 04/Program.cs
@@ -0,0 +1,16 @@
+﻿// Показать четные числа от 1 до N
+Console.WriteLine("Введите число:");
+int max = int.Parse(Console.ReadLine());
+
+int x = 1;
+
+while (x <= max)
+{
+    if (x % 2 == 0)
+    {
+        Console.Write(" " + x);
+    }
+    x++;
+}
+
+
