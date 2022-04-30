92858db8d84d9851903f6969973361ed8842ffb8
Ukolova Nabat <ru.nabat@gmail.com>
2022-04-30 19:27:19 +0400

    zadacha02

---
+++ 02/Program.cs
@@ -0,0 +1,38 @@
+﻿// По заданному номеру дня недели вывести его название
+Console.WriteLine("Введите номер дня недели:");
+int daynumber = int.Parse(Console.ReadLine());
+if (1 == daynumber)
+{
+    Console.WriteLine("Понедельник");
+}
+
+if (2 == daynumber)
+{
+    Console.WriteLine("Вторник");
+}
+
+if (3 == daynumber)
+{
+    Console.WriteLine("Среда");
+}
+
+if (4 == daynumber)
+{
+    Console.WriteLine("Четверг");
+}
+
+if (5 == daynumber)
+{
+    Console.WriteLine("Пятница");
+}
+
+if (6 == daynumber)
+{
+    Console.WriteLine("Суббота");
+}
+
+if (7 == daynumber)
+{
+    Console.WriteLine("Воскресенье");
+}
+
