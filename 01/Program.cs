afe58e6406259d6605e5a038a7be621f2dca3d14
Ukolova Nabat <ru.nabat@gmail.com>
2022-04-30 18:36:28 +0400

    zadacha01

---
+++ 01/Program.cs
@@ -0,0 +1,14 @@
+﻿// По двум заданным числам проверить является ли первое квадратом второго
+Console.Write("Введите число а:");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите число b:");
+int b = int.Parse(Console.ReadLine());
+if (a == b*b)
+{
+    Console.WriteLine("Является");
+}
+else
+{
+    Console.WriteLine("Не является");
+}
+
