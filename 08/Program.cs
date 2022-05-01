6a060552158ba7c9a6d97b3788a5258800da7950
Ukolova Nabat <ru.nabat@gmail.com>
2022-05-01 16:30:34 +0400

    zadacha 08

---
+++ 08/Program.cs
@@ -0,0 +1,14 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток
+Console.WriteLine("Введите проверяемое число:");
+int numX = int.Parse(Console.ReadLine());
+Console.Write("Введите число, кратно которому должно быть проверяемое:");
+int numY  = int.Parse(Console.ReadLine());
+if ((numX  % numY) == 0)
+{
+    Console.WriteLine("$ Число {numX} кратно числу {numY}!");
+}
+
+else
+{
+    Console.WriteLine("$ Число {numX} не кратно числу {numY}, остаток равен {numX % numY}"!);
+}
