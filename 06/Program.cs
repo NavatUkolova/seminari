8ab969c924d1d94041c31dfeecefcafa5304203c
Ukolova Nabat <ru.nabat@gmail.com>
2022-05-01 13:40:39 +0400

    zadacha 06

---
+++ 06/Program.cs
@@ -0,0 +1,23 @@
+﻿// Дано число из отрезка [10, 99].Показать наибольшую цифру числа
+Console.WriteLine("Введите число:");
+int a = int.Parse(Console.ReadLine());
+
+if ( a >=10 && a <= 99)
+{
+    Console.Write("Наибольшее цифра числа:");
+    int second = (a % 10);
+    int first = a / 10;
+    if (first > second)
+    {
+        Console.WriteLine(first);
+    }
+    else
+    {
+        Console.WriteLine(second);
+    }
+
+}
+else
+{
+    Console.WriteLine("Введен неверный диапазон!");
+}
