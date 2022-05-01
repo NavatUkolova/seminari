6fd326fa0a59277db26e6bdc830e7d3e78f198af
Ukolova Nabat <ru.nabat@gmail.com>
2022-05-01 14:21:04 +0400

    zadacha 07

---
+++ 07/Program.cs
@@ -0,0 +1,14 @@
+﻿// Удалить вторую цифру 3-х значного числа
+Console.WriteLine("Введите 3-х значное число:");
+int a = int.Parse(Console.ReadLine());
+
+if (a > 99 && a < 1000)
+{
+    int b = a / 100;
+    int c = a % 10;
+    Console.WriteLine(b * 10 + c);
+}
+else
+{
+    Console.WriteLine("Число не 3-х значное!");
+}
